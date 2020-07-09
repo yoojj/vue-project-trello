f728935942fba766bbc2f8110af5c6ce9b50ecdc
yjj <yoojj.xyz@gmail.com>
2020-07-10 05:02:32 +0900

    NotFound

---
+++ trello/src/views/error/NotFound.vue
@@ -0,0 +1,31 @@
+<template>
+<layout-content>
+    <section class="error-404">
+        <h2>Page not found</h2>
+        <p>{{ $t('error.404') }}</p>
+    </section>
+</layout-content>
+</template>
+
+
+
+<script>
+import { LayoutContent } from '@/layout/'
+
+export default {
+
+    name: 'NotFound',
+
+    components: {
+        LayoutContent,
+    },
+
+}
+</script>
+
+
+
+<style scope>
+.error-404 {text-align:center;}
+.error-404 h2 {margin-bottom:25px;}
+</style>
