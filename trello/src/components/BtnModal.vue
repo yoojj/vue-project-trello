c021f85bcc31090532c38050c752fe13ea218fe1
yjj <yoojj.xyz@gmail.com>
2020-07-09 20:16:58 +0900

    BtnModal

---
+++ trello/src/components/BtnModal.vue
@@ -0,0 +1,51 @@
+<template>
+<span style="display:inline-block;">
+    <button type="button" :class="btnCss" @click="showModal">{{ buttonName }}</button>
+
+    <v-modal :class="this.parentClass" v-if="isModalVisible" @close-modal="isModalVisible=false">
+
+        <template slot="modal-title">
+            <slot name="title"/>
+        </template>
+
+        <template slot="modal-content">
+            <slot name="content"/>
+        </template>
+
+    </v-modal>
+</span>
+</template>
+
+
+
+<script>
+import VModal from '@/components/DefaultModal.vue'
+
+export default {
+
+    name: 'BtnModal',
+
+    components: {
+        VModal,
+    },
+
+    props: [
+        'parentClass',
+        'btnCss',
+        'buttonName'
+    ],
+
+    data() {
+        return {
+            isModalVisible: false,
+        }
+    },
+
+    methods: {
+        showModal() {
+            this.isModalVisible = true;
+        },
+    },
+    
+}
+</script>
