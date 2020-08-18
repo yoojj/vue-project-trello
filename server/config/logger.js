25e6010a5a14a11fe6cd750fc1981d19bc48c638
yjj <yoojj.xyz@gmail.com>
2020-08-18 20:01:10 +0900

    logger.js

---
+++ server/config/logger.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const { createLogger, format, transports } = require('winston');
+const mt = require('moment-timezone');
+
+
+
+const koreaDate = mt().tz('Asia/Seoul');
+const koreaTime = format( info => {
+    info.timestamp = koreaDate.format('hh:mm:ss.SSS');
+    return info;
+});
+
+const myFormat = format.printf((info) =>
+    `${info.timestamp} [${info.level.toUpperCase()}] ${info.label} :: ${info.message}`);
+
+const options = {
+    console: {
+        level: 'info',
+        timestamp: koreaTime,
+        colorize: true,
+        json: false,
+        format: format.combine(
+            format.label({ label: ' ' }),
+            koreaTime(),
+            myFormat,
+        ),
+        handleExceptions: true,
+    },
+
+    file: {
+        level: 'info',
+        timestamp: koreaTime,
+        colorize: false,
+        json: false,
+        filename: path.join(__dirname, `../logs`, koreaDate.format('YYYY-MM-DD'), 'app.log'),
+        maxsize: 5242880,
+        maxFiles: 15,
+        zippedArchive: false,
+        format: format.combine(
+            format.label({ label: ' ' }),
+            myFormat,
+        ),
+        handleExceptions: true,
+    },
+};
+
+
+const logger = createLogger({
+    transports: [
+        new transports.File(options.file),
+    ],
+});
+
+if(process.env.NODE_ENV !== 'production') {
+    logger.add(new transports.Console(options.console));
+}
+
+const morganLogger = {
+    write: (message) => {
+        logger.info(message)
+    },
+};
+
+
+
+exports.logger = logger;
+exports.morganLogger = morganLogger;
