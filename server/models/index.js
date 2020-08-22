1cb14e7ad08fb15fc8f1de0a321143f4ffdadfa9
yjj <yoojj.xyz@gmail.com>
2020-08-22 12:56:24 +0900

    model

--- server/models/index.js
+++ server/models/index.js
@@ -4,28 +4,20 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const config = require('../config/config')[process.env.NODE_ENV];
 
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    port: config.port,
+    dialect: config.dialect,
+    timezone: '+09:00',
+    dialectOptions: {
+        dateStrings: true,
+        typeCast: true,
+    },
+});
+
 const db = {};
-let sequelize;
 
 
-
-if(config.use_env_variable) {
-    sequelize = new Sequelize(
-        process.env[config.use_env_variable],
-        config,
-    );
-
-} else {
-    sequelize = new Sequelize(
-        config.database,
-        config.username,
-        config.password,
-        {
-            host: config.host,
-            dialect: config.dialect,
-        }
-    );
-}
 
 fs.readdirSync(__dirname).filter( file => {
     return (file.indexOf('.' !== 0)) && (file !== 'index.js');
