a0ab59905b06d32a31165b6cc651f24c0c4edb8b
yjj <yoojj.xyz@gmail.com>
2020-08-25 23:29:46 +0900

    valid

---
+++ server/constant/valid.js
@@ -0,0 +1,13 @@
+module.exports = Object.freeze({
+
+    ID : {
+        get min() { return 3 },
+        set min(val) { min = val },
+    },
+
+    PASSWORD : {
+        get min() { return 3 },
+        set min(val) { min = val },
+    },
+
+});
