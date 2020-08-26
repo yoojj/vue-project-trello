fa73d926ad56623081f3407a9d6759f6d78b6df9
yjj <yoojj.xyz@gmail.com>
2020-08-27 06:34:11 +0900

    crypto

---
+++ server/plugin/crypto.js
@@ -0,0 +1,23 @@
+const crypto = require('crypto');
+
+
+
+exports.encryption = (data, key='aes192', hex='base64') => {
+
+    const cipher = crypto.createCipher(key, process.env.CRYPTO_KEY);
+    cipher.update(data, 'utf8', hex);
+
+    return cipher.final(hex).replace('/', '!');
+
+};
+
+exports.decryption = (data, key='aes192', hex='base64') => {
+
+    data.replace('/', '!');
+    
+    const decipher = crypto.createDecipher(key, process.env.CRYPTO_KEY);
+    decipher.update(data, hex, 'utf8');
+
+    return decipher.final('utf-8');
+
+};
