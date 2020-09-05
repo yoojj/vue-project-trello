4e9924fedd3d4ca7889eb50d99043cad94835e08
yjj <yoojj.xyz@gmail.com>
2020-09-05 22:45:44 +0900

    Logout

---
+++ trello/src/views/member/Logout.vue
@@ -0,0 +1,31 @@
+<template>
+<layout-content>
+    <section class="logout-wrap">
+        <h2>Logout</h2>
+        <p>Thanks for using Trello</p>
+    </section>
+</layout-content>
+</template>
+
+
+
+<script>
+import { LayoutContent } from '@/layout/'
+
+export default {
+
+    name: 'Logout',
+
+    components: {
+        LayoutContent,
+    },
+
+}
+</script>
+
+
+
+<style scoped>
+.logout-wrap {text-align:center;}
+.logout-wrap h2 {margin-bottom:25px;}
+</style>
