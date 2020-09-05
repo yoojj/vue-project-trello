0c0ad7d42c2b05627c6b04b4187be936454cb3bd
yjj <yoojj.xyz@gmail.com>
2020-09-05 23:28:08 +0900

    layout

---
+++ trello/src/layout/index.js
@@ -0,0 +1,18 @@
+import LayoutDefault from './LayoutDefault'
+import LayoutContent from './LayoutContent'
+import LayoutUserHome from './LayoutUserHome'
+import LayoutCard from './LayoutCard'
+
+export {
+    LayoutDefault,
+    LayoutContent,
+    LayoutUserHome,
+    LayoutCard,
+};
+
+/*
+LayoutDefault
+    - LayoutContent
+        - LayoutUserHome
+    - LayoutCard
+*/
