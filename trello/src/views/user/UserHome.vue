cc5a7731190e791ff98473bb5912000ddb1b6a91
yjj <yoojj.xyz@gmail.com>
2020-09-08 22:34:55 +0900

    UserHome

---
+++ trello/src/views/user/UserHome.vue
@@ -0,0 +1,33 @@
+<template>
+<layout-user>
+    <section class="">
+        <h1>홈</h1>
+    </section>
+</layout-user>
+</template>
+
+
+
+<script>
+import { LayoutUser } from '@/layout/'
+
+export default {
+
+    name: 'UserHome',
+
+    components: {
+        LayoutUser,
+    },
+
+    data () {
+        return {
+        }
+    },
+
+}
+</script>
+
+
+
+<style scoped>
+</style>
