3647d0a662565ec023aa3176348851a07f39a684
yjj <yoojj.xyz@gmail.com>
2020-09-08 21:10:32 +0900

    LayoutUser

--- trello/src/layout/LayoutUser.vue
+++ trello/src/layout/LayoutUser.vue
@@ -2,9 +2,10 @@
 <layout-content>
 <div class="user-home-layout">
 
-    <nav class="nav-layout">
+    <nav class="nav-layout" :style="{'--nav-width': navWidth}">
         <router-link to="/">홈</router-link>
-        <router-link :to="{ name: 'board-list', params: {} }">보드 리스트</router-link>
+        <router-link to="/profile">프로필</router-link>
+        <router-link to="board-list">보드 리스트</router-link>
     </nav>
 
     <div class="content-layout">
@@ -31,6 +32,27 @@ export default {
         'v-btn-modal': () => import('@/components/BtnModal'),
     },
 
+    data() {
+        return {
+            navWidth: '178px',
+        }
+    },
+
+    mounted() {
+        this.$nextTick( () => {
+            window.addEventListener('resize', this.onResize);
+        });
+    },
+
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+    },
+
+    methods: {
+        onResize() {
+            this.navWidth = (document.querySelector('.nav-layout').offsetWidth) + 'px';
+        }
+    },
 }
 </script>
 
@@ -38,6 +60,7 @@ export default {
 
 <style scoped>
 .user-home-layout {
+    position:relative;
     display:grid;
     grid-template-columns:repeat(14, 1fr);
     grid-template-areas: 'f s';
@@ -46,8 +69,11 @@ export default {
 
 
 /*** 메뉴 ***/
-.nav-layout {grid-area:f;grid-column-start:1;grid-column-end:4;}
+.nav-layout {grid-area:f;grid-column-start:1;grid-column-end:4;
+    min-height:500px;border-right:thin solid #ddd;}
 .nav-layout > a {display:inline-block;width:100%;}
+.nav-layout::after {content:" ";display:block;position:absolute;top:0;left:var(--nav-width);
+    min-height:500px;border-right:thin solid #fff;}
 
 
 /*** 내용 ***/
