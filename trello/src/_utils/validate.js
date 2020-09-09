68babc7398465511b02aec6c49e39e6cc345c282
yjj <yoojj.xyz@gmail.com>
2020-09-09 19:17:46 +0900

    validate

--- trello/src/_utils/validate.js
+++ trello/src/_utils/validate.js
@@ -1,12 +1,46 @@
 export default {
 
-    userForm(data) {
+    value($data, callback){
+
+        if($data.required == true && $data.value.length < 1){
+            $data.error = `${$data.name}을(를) 작성해주세요.`;
+
+        } else if($data.required == true && $data.validated == false){
+            this.valid($data);
+
+        } else if($data.required == true && $data.value.validated == true){
+            return callback(true);
+        }
+
+        return callback($data);
+    },
+
+    object($data, callback){
+        for(let key in $data){
+
+            if($data[key].required == true && $data[key].value.length < 1){
+                $data[key].error = `${$data[key].name}(을/를) 작성해주세요.`;
+
+            } else if($data[key].required == true && $data[key].validated == false){
+                this.valid($data[key]);
+
+            } else if($data[key].required == true && $data[key].validated == true){
+                return callback(true);
+            }
+
+        }
+
+        return callback($data);
+    },
+
+
+    valid(data) {
 
         if(data.value.length < data.min){
             data.error = `${data.min} 글자 이상 입력하세요.`;
 
         } else if(data.pattern.test(data.value) == false){
-            data.error = '정규식 오류';
+            data.error = `${data.name} 형식에 맞지 않습니다.`;
 
         } else {
             data.error = '';
