976833bacb09668e03a36e7c2c65ea767998af1f
yjj <yoojj.xyz@gmail.com>
2020-09-18 06:23:24 +0900

    validate

--- trello/src/_utils/validate.js
+++ trello/src/_utils/validate.js
@@ -1,6 +1,7 @@
 export default {
 
     value($data, callback){
+        $data.validated = false;
 
         if($data.required == true && $data.value.length < 1){
             $data.error = `${$data.name}을(를) 작성해주세요.`;
@@ -16,25 +17,19 @@ export default {
     },
 
     object($data, callback){
-        let result;
-
         for(let key in $data){
+            $data[key].validated = false;
 
             if($data[key].required == true && $data[key].value.length < 1){
-                result = false;
                 $data[key].error = `${$data[key].name}(을/를) 작성해주세요.`;
 
             } else if($data[key].required == true && $data[key].validated == false){
-                result = false;
                 this.valid($data[key]);
-
-            } else if($data[key].required == true && $data[key].validated == true){
-                result = true;
             }
 
         }
 
-        return (result == true) ? callback(true) : callback($data);
+        return callback($data);
     },
 
 
@@ -42,9 +37,11 @@ export default {
 
         if(data.value.length < data.min){
             data.error = `${data.min} 글자 이상 입력하세요.`;
+            data.validated = false;
 
         } else if(data.pattern && data.pattern.test(data.value) == false){
             data.error = `${data.name} 형식에 맞지 않습니다.`;
+            data.validated = false;
 
         } else {
             data.error = '';
