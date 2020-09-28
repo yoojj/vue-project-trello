b9edc60dbb8ddbf977d20e9e7aa90bc19c29c278
yjj <yoojj.xyz@gmail.com>
2020-09-28 16:25:02 +0900

    Logout

--- trello/src/views/member/Logout.vue
+++ trello/src/views/member/Logout.vue
@@ -1,9 +1,11 @@
 <template>
 <layout-content>
-    <section class="logout-wrap">
-        <h2>Logout</h2>
-        <p>Thanks for using Trello</p>
-    </section>
+<section class="logout-wrap">
+    <h1>Logout</h1>
+
+    <p>Thanks for using Trello</p>
+
+</section>
 </layout-content>
 </template>
 
@@ -20,12 +22,35 @@ export default {
         LayoutContent,
     },
 
+
+    created() {
+
+        this.$store.dispatch('LOGOUT'
+        ).then( data => {
+
+            this.$store.commit('deleteBoardList');
+
+            if(self.name != 'reload') {
+                self.name = 'reload';
+                self.location.reload(true);
+            } else {
+                self.name = '';
+            }
+
+        }).catch( err => {
+            alert(err);
+        });
+
+    },
+
+
 }
 </script>
 
 
 
-<style scoped>
-.logout-wrap {text-align:center;}
-.logout-wrap h2 {margin-bottom:25px;}
+<style>
+.logout-wrap {}
+.logout-wrap h1 {margin-bottom:30px;text-align:center;}
+.logout-wrap p {text-align:center;}
 </style>
