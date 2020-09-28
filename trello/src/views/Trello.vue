595124d8c4c11fbe7cd272525264958463a83f67
yjj <yoojj.xyz@gmail.com>
2020-09-28 16:31:58 +0900

    Trello

--- trello/src/views/Trello.vue
+++ trello/src/views/Trello.vue
@@ -1,15 +1,13 @@
 <template>
-    <section class="index-wrap">
-        <h1 class="hide">trello intro</h1>
+<section class="index-wrap">
+    <h1 class="hide">trello intro</h1>
 
-        <section class="index-content-wrap">
-            <nav class="index-nav">
-                <h1><router-link to="/login">Log in to Trello</router-link></h1>
-                <h2><router-link to="/signup">or Sing up</router-link></h2>
-            </nav>
-        </section>
+    <nav class="index-nav">
+        <h1><router-link to="/login">Log in to Trello</router-link></h1>
+        <h2><router-link to="/signup">or Sing up</router-link></h2>
+    </nav>
 
-    </section>
+</section>
 </template>
 
 
@@ -22,14 +20,12 @@ export default {
 
 
 
-<style scoped>
+<style>
 .index-wrap {width:100%;height:100%;
     background-color:#4381b2;background-image:linear-gradient(#4381b2, #4d0099);overflow:hidden;}
 
-.index-content-wrap {width:50%;margin:50px auto 0;}
-
-.index-nav {text-align:center;}
+.index-nav {width:50%;margin:50px auto 0;text-align:center;}
+.index-nav h1 {margin-bottom:25px;font-size:35px;}
 .index-nav a {color:#fff;}
 .index-nav a:hover {color:rgba(255, 255, 255, 0.5);}
-.index-nav h1 {margin-bottom:25px;font-size:35px;}
 </style>
