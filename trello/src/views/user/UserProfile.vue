18a768ef2a116307779252d52682b472471bf35d
yjj <yoojj.xyz@gmail.com>
2020-09-28 16:18:00 +0900

    UserProfile

---
+++ trello/src/views/user/UserProfile.vue
@@ -0,0 +1,35 @@
+<template>
+<layout-user>
+<section class="profile-wrap">
+    <h1 class="">프로필</h1>
+
+</section>
+</layout-user>
+</template>
+
+
+
+<script>
+import { LayoutUser } from '@/layout/'
+
+export default {
+
+    name: 'UserProfile',
+
+    components: {
+        LayoutUser,
+    },
+
+    data () {
+        return {
+        }
+    },
+
+}
+</script>
+
+
+
+<style>
+.profile-wrap {}
+</style>
