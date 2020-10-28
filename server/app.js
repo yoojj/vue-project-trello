9dfcf4b7c77be93b2904dfe2e02db7f93aa621ce
yjj <yoojj.xyz@gmail.com>
2020-10-28 18:40:06 +0900

    app

--- server/app.js
+++ server/app.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
@@ -10,7 +8,6 @@ const morgan = require('morgan');
 const { logger, morganLogger } = require('./config/logger');
 
 const passport = require('passport');
-require('./config/passport');
 
 const db = require('./models');
 db.sequelize.sync();
@@ -25,34 +22,37 @@ const port = process.env.PORT;
 const RESULT = require('./constant/resultCode');
 const token = require('./plugin/token');
 
+const deleteUnauthMail = require('./batch/deleteUnauthMail');
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(helmet());
 app.use(morgan({
     format: env === 'production' ? 'default' : 'dev',
     stream: morganLogger,
 }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+//app.use(helmet.noCache());
+//app.use(helmet.hpkp());
 app.use(cors({ origin: true, credentials: true }));
 app.use(passport.initialize());
 
 
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-
-
 app.use('/auth', require('./routes/auth'));
 app.use('/user', token.verify, require('./routes/user'));
-app.use('/member', token.verify, require('./routes/member'));
-app.use('/card', token.verify, require('./routes/card'));
 app.use('/board', token.verify, require('./routes/board'));
+app.use('/card', token.verify, require('./routes/card'));
+app.use('/content', token.verify, require('./routes/content'));
+app.use('/download', token.verify, require('./routes/download'));
+
+app.use('/admin/members', token.verify, require('./routes/admin/members'));
+app.use('/admin/users', token.verify, require('./routes/admin/users'));
+app.use('/admin/boards', token.verify, require('./routes/admin/boards'));
+app.use('/admin/cards', token.verify, require('./routes/admin/cards'));
+app.use('/admin/replies', token.verify, require('./routes/admin/replies'));
+app.use('/admin/files', token.verify, require('./routes/admin/files'));
 
 
 
@@ -62,17 +62,24 @@ app.use( (req, res, next) => {
 
 app.use( (err, req, res, next) => {
 
-    logger.error(err.stack);
+    let message = null;
 
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    if(typeof err === 'string'){
+        message = err;
+    }
+
+    logger.error(message || err.stack);
+
     res.status(err.status || 500);
+    req.app.get('env') === 'development' ? err : {};
+
     res.json({
         api: req.originalUrl,
         result: {
             boolean: RESULT.ERROR.boolean,
             code: RESULT.ERROR.code,
-            message: RESULT.ERROR.message = err.message,
+            message: message || err.message || RESULT.ERROR.message,
+            object: res.resultObj || null,
         },
     });
 
@@ -81,5 +88,6 @@ app.use( (err, req, res, next) => {
 
 
 app.listen( port, () => {
+    deleteUnauthMail.job();
     console.log('server running at http://localhost:' + port);
 });
